285d4b2e4dbc769dbb8cbc214f727bfcb09cc2dc
tambigangolu <gangolutambi@gmail.com>
2021-08-29 10:54:58 +0530

    first commit

---
+++ src/main/java/com/tambi/UserMangementAppApplication.java
@@ -0,0 +1,13 @@
+package com.tambi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMangementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMangementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tambi/UserMangementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tambi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMangementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
